4296a8c79b247dca02a830399e9056f32d7ddc0c
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-08-29 10:26:10 +0700

    day 2

--- 2. Basic Golang/go.mod
+++ 2. Basic Golang/go.mod
@@ -1,3 +1,7 @@
 module day2
 
 go 1.22.3
+
+// github.com/Yanuarprayoga9/go-devops
+
+require github.com/google/uuid v1.6.0
--- 2. Basic Golang/main.go
+++ 2. Basic Golang/main.go
@@ -1,11 +1,20 @@
-package main 
+package main
 
 import (
-	"day2/shapes"
 	"day2/helper"
+	"day2/shapes"
+	"fmt"
+
+	"github.com/google/uuid"
 )
 
 func main () {
 	shapes.AreaOfCircle(3)
 	helper.PrintHello();
+
+	test := uuid.New().String()
+
+	fmt.Println(test)
+
+	
 }
